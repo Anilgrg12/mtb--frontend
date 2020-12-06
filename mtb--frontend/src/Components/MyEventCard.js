43dd1aed7b6ff6404ddcdb1f23d9967a6ebe80c2
Anil Gurung <anil_grg@hotmail.com>
2020-12-06 18:37:03 -0500

    refactoring old codes

--- mtb--frontend/src/Components/MyEventCard.js
+++ mtb--frontend/src/Components/MyEventCard.js
@@ -1,59 +1,68 @@
-import React, { Component } from 'react'
-import MyEvent from './MyEvent'
+import React, { Component } from "react";
+import MyEvent from "./MyEvent";
 
- class MyEventCard extends Component {
-    
-state = {
-    clicked: false
-}
-toggleClicked = () => {
-    this.setState({clicked: !this.state.clicked})
-}
+class MyEventCard extends Component {
+  state = {
+    clicked: false,
+  };
+  toggleClicked = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
 
-renderShort = () => {
-const{name,description,date_time,img,city,state,website} = this.props.event
-return (
-    <div className = "muji">
-        <div className = "Trails-card">
-             <img style = {{height:330,width:410}}src = {img} /> <br />
-                    <div>Event: {name}</div>
-                        <div>City: {city}</div>
-                                <div>State: {state}</div>
-                                      <div>Website:{website}</div>
-                                         <button className = "fav-event-del">Remove</button>
-                                         
+  renderShort = () => {
+    const {
+      name,
+      description,
+      date_time,
+      img,
+      city,
+      state,
+      website,
+    } = this.props.event;
+    return (
+      <div className="muji">
+        <div className="Trails-card">
+          <img style={{ height: 330, width: 410 }} src={img} /> <br />
+          <div>Event: {name}</div>
+          <div>City: {city}</div>
+          <div>State: {state}</div>
+          <div>Website:{website}</div>
+          <button className="fav-event-del">Remove</button>
         </div>
-        </div>
-)
-}
+      </div>
+    );
+  };
 
-renderFull = () => {
-    const{name,description,date_time,img,city,state,website} = this.props.event
-return( <div className = "Trail-card">
-
-    <div>Name of the event: {name} </div>
-            <div>City: {city}</div>
-                <div>State: {state}</div>
-                     <div>Date: {date_time}</div>
-                             <div>Website: {website}</div>
-                                  <div>Description: {description}</div>
-    </div>
-
-
-)
-
-}
-render() {
-return  (
- <div onClick={this.toggleClicked}>
+  renderFull = () => {
+    const {
+      name,
+      description,
+      date_time,
+      img,
+      city,
+      state,
+      website,
+    } = this.props.event;
+    return (
+      <div className="Trail-card">
+        <div>Name of the event: {name} </div>
+        <div>City: {city}</div>
+        <div>State: {state}</div>
+        <div>Date: {date_time}</div>
+        <div>Website: {website}</div>
+        <div>Description: {description}</div>
+      </div>
+    );
+  };
+  render() {
+    return (
+      <div onClick={this.toggleClicked}>
         <div>
-                 {this.state.clicked? this.renderFull() : this.renderShort()}
-   {/* {this.renderShort()} */}
+          {this.state.clicked ? this.renderFull() : this.renderShort()}
+          {/* {this.renderShort()} */}
         </div>
- </div>
-)
-}
-
-
+      </div>
+    );
+  }
 }
 export default MyEventCard;
--- mtb--frontend/src/Components/TrailForm.js
+++ mtb--frontend/src/Components/TrailForm.js
@@ -1,158 +1,193 @@
-import React, { Component } from 'react'
-import {Form,Button} from 'react-bootstrap'
-const TrailURL = "http://localhost:3000/trails"
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
+const TrailURL = "http://localhost:3000/trails";
 const headers = {
-    "Content-Type": "application/json",
-    Accept: "application/json",
+  "Content-Type": "application/json",
+  Accept: "application/json",
+};
+
+export default class TrailForm extends Component {
+  state = {
+    name: "",
+    description: "",
+    length: "",
+    difficulty: "",
+    stars: "",
+    high: "",
+    location: "",
+    low: "",
+    image_url: "",
   };
 
-export default class TrailForm extends Component{
-    
-    state = {
-        name: '',
-        description:'',
-        length:'',
-        difficulty:'',
-        stars:'',
-        high:'',
-        location:'',
-        low:'',
-        image_url:'',
-    
-     }
+  handleChanged = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
 
-    handleChanged = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
 
-
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-    
     fetch(TrailURL, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept':'application/json'
-        },
-        body: JSON.stringify ({
-            name: this.state.name,
-            description: this.state.description,
-            length:this.state.length,
-            difficulty:this.state.difficulty,
-            stars:this.state.stars,
-            high:this.state.high,
-            low:this.state.low,
-            comment:this.state.comment,
-            location:this.state.location,
-            image_url:this.state.image_url
-
-
-        })
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: this.state.name,
+        description: this.state.description,
+        length: this.state.length,
+        difficulty: this.state.difficulty,
+        stars: this.state.stars,
+        high: this.state.high,
+        low: this.state.low,
+        comment: this.state.comment,
+        location: this.state.location,
+        image_url: this.state.image_url,
+      }),
     })
-
-        .then(resp => resp.json())
-        .then ((data) => this.setState({
-            trails: [this.state.trails, data]
-        })
-        )
-        
-        this.resetForm();
-        // alert("Thank you for your contribution. Your trail has beed added")
-        this.props.history.push('/')
-    }
-
-    resetForm = () => {
+      .then((resp) => resp.json())
+      .then((data) =>
         this.setState({
-        name: '',
-        description:'',
-        length:'',
-        stars:'',
-        difficulty:'',
-        rating:'',
-        high:'',
-        low:'',
-        image_url:'',
-        location:''
+          trails: [this.state.trails, data],
         })
+      );
 
-    }
+    this.resetForm();
+    // alert("Thank you for your contribution. Your trail has beed added")
+    this.props.history.push("/");
+  };
 
-    
+  resetForm = () => {
+    this.setState({
+      name: "",
+      description: "",
+      length: "",
+      stars: "",
+      difficulty: "",
+      rating: "",
+      high: "",
+      low: "",
+      image_url: "",
+      location: "",
+    });
+  };
 
-    render(){
-         console.log(this.state)
+  render() {
+    console.log(this.state);
 
     return (
-        <div className = "about-image">
-        <img className = "about-create" src = "https://i.dmarge.com/2017/02/bontrager-960x580.jpg" />
-        <div className = "about-form">
-    <form onSubmit = {this.handleSubmit} >
+      <div className="about-image">
+        <img
+          className="about-create"
+          src="https://i.dmarge.com/2017/02/bontrager-960x580.jpg"
+        />
+        <div className="about-form">
+          <form onSubmit={this.handleSubmit}>
             <label>
-            <Form.Group controlId="formBasicEmail">
-            <Form.Label>           
-            Trail Name: </Form.Label>
-            <input type="text" size ="80" className = "form-control" name = "name" value = {this.state.name} placeholder = "Enter trail name" onChange = {this.handleChanged} />
-            </Form.Group>
-            <Form.Group>
-            <Form.Label>
-            Trail Image: </Form.Label>
-            <Form.Control placeholder = "Upload trail image"  src = {this.state.image_url} name = "image_url" onChange = {this.handleChanged} />
-            </Form.Group>
-            <Form.Group>
-            <Form.Label>
-            Difficulty:</Form.Label>
-            <div>
-            <select className = "form-control" placeholder = "Select difficulty level" value = {this.state.difficulty} name = "difficulty" onChange = {this.handleChanged}>
-            <option>Select difficulty level</option>
-            <option>Easy</option>
-            <option>Intermediate</option>
-            <option>Hard</option>
-            </select>
-            </div>
-            </Form.Group>
-            <Form.Group>
-            <Form.Label>
-             Rating:</Form.Label>
-            <select className = "form-control" placeholder = "Enter Rating" value = {this.state.stars} name = "stars" onChange = {this.handleChanged}>
-            <option>Select your rating</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            </select>
-            </Form.Group>
-            <Form.Group>
-            <Form.Label>
-            Length: </Form.Label>
-            <Form.Control type = "integer" name = "length" value = {this.state.length} placeholder = "Enter the distance" onChange = {this.handleChanged}/> 
-            </Form.Group>
-            <Form.Group>
-            <Form.Label>
-            Description: </Form.Label>
-            <Form.Control type="text"  name = "description" value = {this.state.description} placeholder = "Enter the description" onChange = {this.handleChanged} />
-            </Form.Group>
-            <Form.Group>
-            <Form.Label>
-             High: </Form.Label>
-            <Form.Control type="integer" name = "high"value = {this.state.high} placeholder = "Enter High" onChange = {this.handleChanged}/>
-            </Form.Group>
-            <Form.Group>
-            <Form.Label>
-            Low:</Form.Label>
-            <Form.Control type="integer" name = "low"value = {this.state.low} placeholder = "Enter Low" onChange = {this.handleChanged}/>
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                                                  Submit
-                                                        </Button>
-         </label> 
-            </form>
-         </div>  
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Trail Name: </Form.Label>
+                <input
+                  type="text"
+                  size="80"
+                  className="form-control"
+                  name="name"
+                  value={this.state.name}
+                  placeholder="Enter trail name"
+                  onChange={this.handleChanged}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Trail Image: </Form.Label>
+                <Form.Control
+                  placeholder="Upload trail image"
+                  src={this.state.image_url}
+                  name="image_url"
+                  onChange={this.handleChanged}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Difficulty:</Form.Label>
+                <div>
+                  <select
+                    className="form-control"
+                    placeholder="Select difficulty level"
+                    value={this.state.difficulty}
+                    name="difficulty"
+                    onChange={this.handleChanged}
+                  >
+                    <option>Select difficulty level</option>
+                    <option>Easy</option>
+                    <option>Intermediate</option>
+                    <option>Hard</option>
+                  </select>
+                </div>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Rating:</Form.Label>
+                <select
+                  className="form-control"
+                  placeholder="Enter Rating"
+                  value={this.state.stars}
+                  name="stars"
+                  onChange={this.handleChanged}
+                >
+                  <option>Select your rating</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </select>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Length: </Form.Label>
+                <Form.Control
+                  type="integer"
+                  name="length"
+                  value={this.state.length}
+                  placeholder="Enter the distance"
+                  onChange={this.handleChanged}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Description: </Form.Label>
+                <Form.Control
+                  type="text"
+                  name="description"
+                  value={this.state.description}
+                  placeholder="Enter the description"
+                  onChange={this.handleChanged}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>High: </Form.Label>
+                <Form.Control
+                  type="integer"
+                  name="high"
+                  value={this.state.high}
+                  placeholder="Enter High"
+                  onChange={this.handleChanged}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Low:</Form.Label>
+                <Form.Control
+                  type="integer"
+                  name="low"
+                  value={this.state.low}
+                  placeholder="Enter Low"
+                  onChange={this.handleChanged}
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </label>
+          </form>
         </div>
-    )
-}
+      </div>
+    );
+  }
 }
