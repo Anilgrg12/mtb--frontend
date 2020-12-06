2a755148da88e66ac4da9ba7e10f7224fdc68d0a
Anil Gurung <anil_grg@hotmail.com>
2020-12-06 18:40:47 -0500

    removed unused codes:

--- mtb--frontend/src/Components/Trails.js
+++ mtb--frontend/src/Components/Trails.js
@@ -1,48 +1,47 @@
-// import React from 'react';
-// import { useHistory } from 'react-router-dom';
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Trails extends Component {
-    
-    
-      handleTrail = (e) => {
-            e.preventDefault()
-            let user_id = localStorage.getItem("user_id")
-            let trail_id = this.props.trail.id
-            // debugger
-    
-            fetch("http://localhost:3000/fav_trails/", { 
-            method: "POST",
-            headers: {
-                "content-type": "application/json",
-                accepts: "application/json"
-            },
-            body: JSON.stringify({ 
-                user_id: user_id,
-                trail_id: trail_id
-            })
-        })
-            .then(resp => resp.json())
-            .then(resp => console.log(resp))
-        
-        
-        }
-    render() {
-        // console.log("ghnata",this.props)
-        const {id, name,difficulty,length,image_url} = this.props.trail
-     return (
-         <div className = "Trail-card">
-        <img className = "image" src = {image_url} alt = {name}></img>
+  handleTrail = (e) => {
+    e.preventDefault();
+    let user_id = localStorage.getItem("user_id");
+    let trail_id = this.props.trail.id;
+
+    fetch("http://localhost:3000/fav_trails/", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        accepts: "application/json",
+      },
+      body: JSON.stringify({
+        user_id: user_id,
+        trail_id: trail_id,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((resp) => console.log(resp));
+  };
+  render() {
+    // console.log("ghnata",this.props)
+    const { id, name, difficulty, length, image_url } = this.props.trail;
+    return (
+      <div className="Trail-card">
+        <img className="image" src={image_url} alt={name}></img>
         Name: {name} <br />
         Difficulty: {difficulty} <br />
         Length: {length} miles
         {/* <button className = "delete" onClick = {()=> this.props.deleteHandler(id)}>❌</button> */}
-        <button className = "button" onClick = {() => this.props.history.push(`/trails/${id}`)}> Details </button>
-        <button className = "fav-button" onClick = {this.handleTrail}>❤️</button>
-
-        </div>
-
-    )
-}
+        <button
+          className="button"
+          onClick={() => this.props.history.push(`/trails/${id}`)}
+        >
+          {" "}
+          Details{" "}
+        </button>
+        <button className="fav-button" onClick={this.handleTrail}>
+          ❤️
+        </button>
+      </div>
+    );
+  }
 }
 export default Trails;
