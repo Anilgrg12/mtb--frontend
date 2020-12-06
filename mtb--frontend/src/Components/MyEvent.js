3747a40b39a5d4c1f857d9e3f685b29a70c9f1a2
Anil Gurung <anil_grg@hotmail.com>
2020-12-06 18:39:50 -0500

    myeventscomponent

--- mtb--frontend/src/Components/MyEvent.js
+++ mtb--frontend/src/Components/MyEvent.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react'
-import MyEventCard from './MyEventCard'
-const EventURL = 'http://localhost:3000/events'
+import React, { Component } from "react";
+import MyEventCard from "./MyEventCard";
+const EventURL = "http://localhost:3000/events";
 
 export default class MyEvent extends Component {
-   state = {
-       myEvents : []
-   }
+  state = {
+    myEvents: [],
+  };
 
-   componentDidMount(){
-        let userId = localStorage.getItem("user_id");
+  componentDidMount() {
+    let userId = localStorage.getItem("user_id");
 
-       fetch(`http://localhost:3000/users/${userId}/add_event`)
-       .then(resp => resp.json())
-       .then(myEvents => { this.setState({
-           myEvents
-       })})
-   }
+    fetch(`http://localhost:3000/users/${userId}/add_event`)
+      .then((resp) => resp.json())
+      .then((myEvents) => {
+        this.setState({
+          myEvents,
+        });
+      });
+  }
 
-    render() {
-       console.log("gedaa",this.state)
-        return (
-            <div>
-                {this.state.myEvents.map(event => <MyEventCard key = {event.id} event = {event} />)}
-            </div>
-        )
-    }
+  render() {
+    // console.log("gedaa", this.state);
+    return (
+      <div>
+        {this.state.myEvents.map((event) => (
+          <MyEventCard key={event.id} event={event} />
+        ))}
+      </div>
+    );
+  }
 }
