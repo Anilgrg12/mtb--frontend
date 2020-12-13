8484d02a184eddfc5c54e995d730626e564c47b5
Anil Gurung <anil_grg@hotmail.com>
2020-12-12 23:42:11 -0500

    made chages to home componwnt

--- mtb--frontend/src/Components/Home.js
+++ mtb--frontend/src/Components/Home.js
@@ -1,63 +1,69 @@
-import React, { Component } from 'react'
-import TrailList from './TrailList'
-import Picture from './Picture'
-import SearchBar from './SearchBar'
-import TrailDetail from './TrailDetail'
-import SortTrail from './SortTrail'
-import TrailForm from './TrailForm'
-const TrailURL = "http://localhost:3000/trails"
+import React, { Component } from "react";
+import TrailList from "./TrailList";
+import Picture from "./Picture";
+import SearchBar from "./SearchBar";
+import TrailDetail from "./TrailDetail";
+import SortTrail from "./SortTrail";
+import TrailForm from "./TrailForm";
+const TrailURL = "http://localhost:3000/trails";
 const headers = {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  };
-
+  "Content-Type": "application/json",
+  Accept: "application/json",
+};
 
 export default class Home extends Component {
+  state = {
+    trails: [],
+    searchTerm: "",
+    sortBy: "",
+  };
 
-state = {
-      trails: [],
-      searchTerm: '',
-      sortBy: ''
-  }
-
-searchHandler = (event) =>{
+  searchHandler = (event) => {
     this.setState({
-    searchTerm: event.target.value
-    })
-  }
+      searchTerm: event.target.value,
+    });
+  };
 
-sortOption = (event) => {
+  sortOption = (event) => {
     this.setState({
-    sortBy: event.target.value
-    })
-  }
+      sortBy: event.target.value,
+    });
+  };
 
-filterTrail = (type) => {
-    let sorted = []
-    if (type === 'all') {
-      sorted = [...this.state.trails]
-    } else if (type === 'name') {
-      sorted = [...this.state.trails].sort((a, b) => a.name > b.name ? 1 : -1)
-    } else if (type === 'description') {
-      sorted = [...this.state.trails].sort((a, b) => a.description > b.description ? 1 : -1)
-    } else if (type === 'length') {
-       sorted = [...this.state.trails].sort((a, b) => a.length > b.length ? 1 : -1)
-      } 
-      else if (type === 'stars') {
-        sorted = [...this.state.trails].sort((a, b) => a.stars > b.stars ? 1 : -1)
-      }
-      else if (type === 'location') {
-        sorted = [...this.state.trails].sort((a, b) => a.location > b.location ? 1 : -1)
-      }  
-      else if (type === 'high') {
-        sorted = [...this.state.trails].sort((a, b) => a.high > b.high ? 1 : -1)
-      } 
-      else if (type === 'low') {
-        sorted = [...this.state.trails].sort((a, b) => a.low > b.low ? 1 : -1)
-      } 
+  filterTrail = (type) => {
+    let sorted = [];
+    if (type === "all") {
+      sorted = [...this.state.trails];
+    } else if (type === "name") {
+      sorted = [...this.state.trails].sort((a, b) =>
+        a.name > b.name ? 1 : -1
+      );
+    } else if (type === "description") {
+      sorted = [...this.state.trails].sort((a, b) =>
+        a.description > b.description ? 1 : -1
+      );
+    } else if (type === "length") {
+      sorted = [...this.state.trails].sort((a, b) =>
+        a.length > b.length ? 1 : -1
+      );
+    } else if (type === "stars") {
+      sorted = [...this.state.trails].sort((a, b) =>
+        a.stars > b.stars ? 1 : -1
+      );
+    } else if (type === "location") {
+      sorted = [...this.state.trails].sort((a, b) =>
+        a.location > b.location ? 1 : -1
+      );
+    } else if (type === "high") {
+      sorted = [...this.state.trails].sort((a, b) =>
+        a.high > b.high ? 1 : -1
+      );
+    } else if (type === "low") {
+      sorted = [...this.state.trails].sort((a, b) => (a.low > b.low ? 1 : -1));
+    }
 
-    return sorted
-  }
+    return sorted;
+  };
 
   deleteHandler = (id) => {
     fetch(TrailURL + "/" + id, {
@@ -74,42 +80,48 @@ filterTrail = (type) => {
       .catch((err) => console.log(err));
   };
 
-componentDidMount(){
-    document.body.style.backgroundColor = " black"
-      fetch('http://localhost:3000/trails')
-      .then(resp => resp.json())
-      .then(trails => {
-              this.setState({trails})
-      }
-
-      )
+  componentDidMount() {
+    document.body.style.backgroundColor = " black";
+    fetch("http://localhost:3000/trails")
+      .then((resp) => resp.json())
+      .then((trails) => {
+        this.setState({ trails });
+      });
   }
-  
-render() {
-// console.log('hello',this.props)
 
-
-let FilteredTrail = this.state.trails.filter(trail => trail.name.toLowerCase().includes (this.state.searchTerm.toLowerCase()))
-let sorted = this.filterTrail(this.state.sortBy)
-return (
-    <div className = "home-page">
+  render() {
+    // console.log('hello',this.props)
+    let FilteredTrail = this.state.trails.filter((trail) =>
+      trail.name.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+    );
+    let sorted = this.filterTrail(this.state.sortBy);
+    return (
+      <div className="home-page">
         <Picture />
-        <div className = "search-sort">
-        <SearchBar searchHandler = {this.searchHandler} />
+        <div className="search-sort">
+          <SearchBar searchHandler={this.searchHandler} />
         </div>
-        <div className = "sort-search">
-        <SortTrail type={this.state.sortBy} handleChange={this.sortOption}/>
-        {this.state.sortBy
-        ? <TrailList style = {{backgroundColor:"blue"}} trails={sorted} deleteHandler={this.deleteHandler} history = {this.props.history}/>
-        : <TrailList className = "trails-page" trails={FilteredTrail} deleteHandler={this.deleteHandler} history = {this.props.history}/>
-        }
+        <div className="sort-search">
+          <SortTrail type={this.state.sortBy} handleChange={this.sortOption} />
+          {this.state.sortBy ? (
+            <TrailList
+              style={{ backgroundColor: "blue" }}
+              trails={sorted}
+              deleteHandler={this.deleteHandler}
+              history={this.props.history}
+            />
+          ) : (
+            <TrailList
+              className="trails-page"
+              trails={FilteredTrail}
+              deleteHandler={this.deleteHandler}
+              history={this.props.history}
+            />
+          )}
         </div>
 
         {/* <TrailForm onChange = {this.handleChanged} onSubmit = {this.handleSubmit} /> */}
-         
-    
-        
-    </div>
-        )
-    }
+      </div>
+    );
+  }
 }
--- mtb--frontend/src/Components/Login.js
+++ mtb--frontend/src/Components/Login.js
@@ -1,72 +1,76 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Login extends Component {
-    
-    state = {
-        email: '',
-        password: ''
-    }
+  state = {
+    email: "",
+    password: "",
+  };
 
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-    handleChange = (event) => { this.setState ({[event.target.name]: event.target.value})
-        
-    }
+  submitHandler = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3000/sessions", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        accepts: "application/json",
+      },
+      body: JSON.stringify({
+        password: this.state.password,
+        email: this.state.email,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((resp) => {
+        if (resp.errors) {
+          console.log("erorr");
+        } else {
+          localStorage.setItem("user_id", resp.id);
+          //   debugger
+          this.props.history.push("/");
+        }
+      });
+  };
 
-    
-    submitHandler = (e) => {
-        e.preventDefault()
-        fetch("http://localhost:3000/sessions", {
-            method: "POST",
-            headers: {
-                "content-type": "application/json",
-                accepts: "application/json"
-            },
-            body: JSON.stringify({
-                password: this.state.password,
-                email: this.state.email
-            })
-        })
-            .then(resp => resp.json())
-            .then(resp => {
-                if (resp.errors) {
-                    console.log("erorr")
-                }
-                else {
-                    localStorage.setItem("user_id", resp.id);
-                    //   debugger
-                    this.props.history.push("/");
-                }
-
-            })
-            
-
-    }
-   
-    render(){
-    
-        return (
-    
-                <div className = "about-image">
-                  <img className = "about" src ="https://trek.scene7.com/is/image/TrekBicycleProducts/b300_mtbMarqueeImage?$responsive-pjpg$&cache=on,on&wid=1920"></img>
-                        <div className = "login-input">
-                            <form onSubmit={this.submitHandler}>
-                                <input placeholder="Enter Email Address" value = {this.state.email} name = "email" onChange={this.handleChange} /> <br />
-                                <input placeholder="Enter Password" type = "password" value = {this.state.password} name = "password" onChange= {this.handleChange}/> <br />
-                                    <div className = "login-buton">
-                                         <button>Log In</button> <br /> <br />
-
-                                    <Link className = "signup-button" to="/signup">Sign Up</Link>
-                                    </div>
-                                        
-                                    
-                            </form>
-                        </div>
-                </div>
-           
-    )
-}
+  render() {
+    return (
+      <div className="about-image">
+        <img
+          className="about"
+          src="https://trek.scene7.com/is/image/TrekBicycleProducts/b300_mtbMarqueeImage?$responsive-pjpg$&cache=on,on&wid=1920"
+        ></img>
+        <div className="login-input">
+          <form onSubmit={this.submitHandler}>
+            <input
+              placeholder="Enter Email Address"
+              value={this.state.email}
+              name="email"
+              onChange={this.handleChange}
+            />{" "}
+            <br />
+            <input
+              placeholder="Enter Password"
+              type="password"
+              value={this.state.password}
+              name="password"
+              onChange={this.handleChange}
+            />{" "}
+            <br />
+            <div className="login-buton">
+              <button>Log In</button> <br /> <br />
+              <Link className="signup-button" to="/signup">
+                Sign Up
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
