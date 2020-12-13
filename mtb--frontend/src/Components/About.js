f7b119a7615911346b8f43b18423e50a2a6e71d5
Anil Gurung <anil_grg@hotmail.com>
2020-12-12 23:44:34 -0500

    made changes to favorite component

--- mtb--frontend/src/Components/About.js
+++ mtb--frontend/src/Components/About.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return(
-        <div className = "about-image">
-            <img className = "about" src ="https://www.newzealandsingletrackadventures.com/wp-content/uploads/2016/11/IMG_1011.jpg" />
-            <div className = "about-des">MTB is a website built with a mission to increase and strengthen the diversity of the mountain biking community.With all the information provided in the website, you will be able to choose the correct bike trail that matches with your choice.</div>
-            <div className = "about-des1">  "Life is better on the mountain bike."</div>
-        </div>
-    )
-}
+  return (
+    <div className="about-image">
+      <img
+        className="about"
+        src="https://www.newzealandsingletrackadventures.com/wp-content/uploads/2016/11/IMG_1011.jpg"
+      />
+      <div className="about-des">
+        MTB is a website built with a mission to increase and strengthen the
+        diversity of the mountain biking community.With all the information
+        provided in the website, you will be able to choose the correct bike
+        trail that matches with your choice.
+      </div>
+      <div className="about-des1"> "Life is better on the mountain bike."</div>
+    </div>
+  );
+};
 
 export default About;
